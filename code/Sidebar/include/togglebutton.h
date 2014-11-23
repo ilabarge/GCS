7f757105381174f648b20d46cf974ae9170867ed
Ethan Ito <ethan.ito.rhs@gmail.com>
2014-11-23 15:17:33 -0800

    Moved sidebar ouside GCS folder to general code folder

---
+++ code/Sidebar/include/togglebutton.h
@@ -0,0 +1,29 @@
+#ifndef TOGGLEBUTTON_H
+#define TOGGLEBUTTON_H
+#include <QtWidgets>
+#include <QMainWindow>
+
+class toggleButton : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit toggleButton(QWidget *parent = 0);
+    void setIcon(QIcon);
+    void setIconSize(QSize);
+    bool checkVisibility();
+     ~toggleButton();
+
+
+signals:
+    void toggled();
+
+private slots:
+    void check();
+
+private:
+    QPushButton *button;
+    bool visibility;
+};
+
+#endif // TOGGLEBUTTON_H
