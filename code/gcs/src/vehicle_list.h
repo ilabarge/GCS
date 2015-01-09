62cac90405de2c6f46903b20817e5c9a16b048da
Ethan Ito <ethan.ito.rhs@gmail.com>
2015-01-08 22:41:44 -0800

    vehicle_list files were not added to be tracked

---
+++ code/gcs/src/vehicle_list.h
@@ -0,0 +1,31 @@
+#ifndef VEHICLE_LIST_H
+#define VEHICLE_LIST_H
+#include <Vehicle22.h>
+#include <qdebug.h>
+#include <vector>
+#include <Qobject>
+class vehicle_list : public QObject
+{
+    Q_OBJECT
+public:
+    explicit vehicle_list(QObject *parent = 0);
+    Vehicle22 *at(int);
+    Vehicle22 *set(int);
+    QVector<int> getList();
+    std::vector<Vehicle22 *> list();
+    void append(Vehicle22 *);
+    void updateVech(int);
+    int length();
+
+signals:
+    //Vehicle id to update
+    void update(int);
+    void incorrect(int);
+
+private:
+    std::vector<Vehicle22*> vlist;
+    std::vector<int> pos;
+    int indexOf(int);
+};
+
+#endif // VEHICLE_LIST_H
